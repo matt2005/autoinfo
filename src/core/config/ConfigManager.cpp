d6603c88b36524e4a1ad7f167223a918c16770d6
matt2005 <matt2005@users.noreply.github.com>
2025-11-24 22:26:54 +0000

    style: Apply clang-format to ConfigManager.cpp for CI parity

--- src/core/config/ConfigManager.cpp
+++ src/core/config/ConfigManager.cpp
@@ -645,13 +645,13 @@ bool ConfigManager::compressToFile(const QByteArray& data, const QString& filePa
     file.write(compressed);
     file.close();
 
-    const QString backupMsg =
-        QString("Backed up config to compressed file: %1 "
-                "Original: %2 bytes "
-                "Compressed: %3 bytes")
-            .arg(filePath)
-            .arg(data.size())
-            .arg(compressed.size());
+    const QString backupMsg = QString(
+                                  "Backed up config to compressed file: %1 "
+                                  "Original: %2 bytes "
+                                  "Compressed: %3 bytes")
+                                  .arg(filePath)
+                                  .arg(data.size())
+                                  .arg(compressed.size());
     qInfo() << backupMsg;
     return true;
 }
@@ -674,13 +674,13 @@ QByteArray ConfigManager::decompressFromFile(const QString& filePath, bool& succ
         return QByteArray();
     }
 
-    const QString decompressMsg =
-        QString("Decompressed config from: %1 "
-                "Compressed: %2 bytes "
-                "Decompressed: %3 bytes")
-            .arg(filePath)
-            .arg(compressed.size())
-            .arg(decompressed.size());
+    const QString decompressMsg = QString(
+                                      "Decompressed config from: %1 "
+                                      "Compressed: %2 bytes "
+                                      "Decompressed: %3 bytes")
+                                      .arg(filePath)
+                                      .arg(compressed.size())
+                                      .arg(decompressed.size());
     qDebug() << decompressMsg;
 
     success = true;
